92c54e5dd435a3023ec3089586955dbe35d25566
yes123-123 <tchampneys2@gmail.com>
2025-11-10 23:47:49 -0700

    Update cube.lua

--- cube.lua
+++ cube.lua
@@ -1,4 +1,16 @@
 print("Spawning cube")
 graphics.new("cube", {x=0, y=1, z=0})
+wait(1)
+print("wait function test")
+wait(1)
+print("wait function test")
+wait(1)
+print("wait function test")
+wait(1)
+print("wait function test")
+wait(1)
+print("wait function test")
+wait(1)
+print("wait function test")
 
 
